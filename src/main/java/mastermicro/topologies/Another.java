6ddb7204c7f73e6f636017e4e12d32748b43ac47
محمد الصوالحي <ms.2052001@gmail.com>
2022-05-08 19:58:07 +0200

    Setup maven+java project with JUnit5 for test

---
+++ src/main/java/mastermicro/topologies/Another.java
@@ -0,0 +1,8 @@
+package mastermicro.topologies;
+import static mastermicro.topologies.Main.i;
+
+public class Another {
+  public static void main(String[] args) {
+    System.out.println(i);
+  }
+}
---
+++ src/main/java/mastermicro/topologies/Main.java
@@ -0,0 +1,9 @@
+package mastermicro.topologies;
+
+public class Main {
+  public static final int i = 0;
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
